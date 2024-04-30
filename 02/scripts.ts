a9362ee8d2296f7426f146fc625c8cd074d2e080
Igor Lacorte <igor.lacorte@gmail.com>
2024-04-30 12:06:03 -0300

    Exercício finalizado

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,23 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    rua: string,
+    cep: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type TipoTransacao = Lowercase<'credito' | 'Debito'>
+
+type NovoCarrinho = {
+    item: Item,
+    qtd: number,
+    desconto: number,
+    frete: number,
+    tipoTransacao: TipoTransacao,
+    cartao: Cartao,
+    endereco: Endereco
+}
